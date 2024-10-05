06bf7d69287877adb3b7b5aa075eab53bfa28329
Kanty Gakima <kantylouange.gakima@gmail.com>
2024-10-05 00:08:26 -0400

    Update translator.py

--- python/translator.py
+++ python/translator.py
@@ -1,73 +1,106 @@
 import sys
 
 braille_alphabet= {
-    'a':'0.....', 'b':'0.0...', 'c':'00....', 'd':'00.0..','e':'0..0..',
-    'f':'000...', 'g':'0000..', 'h':'0.00..', 'i':'.00...', 'j':'.000..',
-    'k':'0...0.', 'l':'0.0.0.', 'm':'00..0.', 'n':'00.00.', 'o':'0..00.',
-    'p':'000.0.', 'q':'00000.', 'r':'0.000.', 's':'.00.0.', 't':'.0000.',
-    'u':'0...00', 'v':'0.0.00', 'w':'.000.0', 'x':'00..00', 'y':'00.000', 'z':'0..000',
-    '1':'0.....', '2':'0.0...', '3':'00....', '4':'00.0..', '5':'0..0..',
-    '6':'000...', '7':'0000..', '8':'0.00..', '9':'.00...', '0':'.000..',
-    'capital':'.....0', 'decimal':'.0...0', 'number':'.0.00.',' ':'......', 
-    '.':'..00.0', ',':'..0...', '?':'..0.00', '!':'..00.0', ':':'..00..',
-    ';':'..0.0.', '-':'....00', '/':'.0..0.', '<':'.00..0', '>':'0..00.',
-    '(':'0.0..0', ')':'.0.00.'}
+    'a':'O.....', 'b':'O.O...', 'c':'OO....', 'd':'OO.O..', 'e':'O..O..',
+    'f':'OOO...', 'g':'OOOO..', 'h':'O.OO..', 'i':'.OO...', 'j':'.OOO..',
+    'k':'O...O.', 'l':'O.O.O.', 'm':'OO..O.', 'n':'OO.OO.', 'o':'O..OO.',
+    'p':'OOO.O.', 'q':'OOOOO.', 'r':'O.OOO.', 's':'.OO.O.', 't':'.OOOO.',
+    'u':'O...OO', 'v':'O.O.OO', 'w':'.OOO.O', 'x':'OO..OO', 'y':'OO.OOO',
+    'z':'O..OOO'}
+
+braille_numbers_and_symbols = {
+    '1':'O.....', '2':'O.O...', '3':'OO....', '4':'OO.O..', '5':'O..O..',
+    '6':'OOO...', '7':'OOOO..', '8':'O.OO..', '9':'.OO...', 'O':'.OOO..',
+    'capital':'.....O', 'decimal':'.O...O', 'number':'.O.OOO',' ':'......', 
+    '.':'..OO.O', ',':'..O...', '?':'..O.OO', '!':'..OO.O', ':':'..OO..',
+    ';':'..O.O.', '-':'....OO', '/':'.O..O.', '<':'.OO..O', '>':'O..OO.',
+    '(':'O.O..O', ')':'.O.OO.'}
     
 english_alphabet = {i: j for j, i in braille_alphabet.items()}
+symbols_and_numbers = {i: j for j, i in braille_numbers_and_symbols.items()}
 
 
-def englist_to_braille(text):
+def english_to_braille(text):
     braille = []
+    prev_char = None # previous character to track the previous character of a number
     for i in text:
         if i.isdigit():
-            braille.append(braille_alphabet['number'])
-            braille.append(braille_alphabet[i])
+            if prev_char == ' ' or prev_char == None:
+                braille.append(braille_numbers_and_symbols['number'])
+            braille.append(braille_numbers_and_symbols[i])
         elif i.isalpha():
             if i.isupper():
-                braille.append(braille_alphabet['capital'])
-                braille.append(braille_alphabet[i.lower()])
+                braille.append(braille_numbers_and_symbols['capital'])
             braille.append(braille_alphabet[i.lower()])
         elif i == ' ':
+            braille.append(braille_numbers_and_symbols[' '])
+
+        elif i in braille_numbers_and_symbols:
+            braille.append(braille_numbers_and_symbols[i])
+        else:
+
             braille.append(braille_alphabet[i])
-        elif i in braille_alphabet:
-            braille.append(braille_alphabet[i])
+        prev_char = i
+
     return ''.join(braille)
 
 def braille_to_english(braille):
     txt = []
     is_number = False
-    for i in range(0, len(braille),6):
-        char = braille[i:i+6]
+    i = 0
+    while i < len(braille):
+        char =  braille[i:i+6]
 
-        if char == braille_alphabet['capital']:
-            i+=6
+        #check if the character is a capital letter
+        if char ==braille_numbers_and_symbols['capital']:
+            i += 6
             nxt_char = braille[i:i+6]
-            txt.append(english_alphabet[nxt_char].upper())
-        elif char == braille_alphabet['number']:
+            txt.append(english_alphabet.get(nxt_char,'#').upper())
+
+        elif char == braille_numbers_and_symbols['number']:
             is_number = True
-        elif is_number and char in english_alphabet:
-            txt.append(english_alphabet[char])
+
+        elif is_number and char in symbols_and_numbers:
+            txt.append(symbols_and_numbers[char])
+            if char == braille_numbers_and_symbols[' ']:
+                is_number = False
+
         elif char in english_alphabet:
             txt.append(english_alphabet[char])
 
-        if is_number and char= == '......':
-            is_number = False
+        elif char in symbols_and_numbers:
+            txt.append(symbols_and_numbers[char])
+        else:
+            txt.append('#')
+
+        i += 6
     return ''.join(txt)
 
             
-    
+   
 #function to check if the text is in braille
 def is_braille(text):
-    return all(i in '0.' for i in text) and len(text) % 6 == 0
+    if len(text) % 6 != 0:
+        return False
+    
+    for i in range(0, len(text), 6):
+        char = text[i:i+6]
+        if char not in english_alphabet and char not in symbols_and_numbers:
+            return False
+    return True
 
 def main():
     if len(sys.argv) < 2:
-        print('Write a text to convert to braille or braille to text')
         sys.exit(1)
 
-    if is_braille(sys.argv[1]):
-        print(braille_to_english(sys.argv[1]))
+    #join all the arguments into a single string
+    string = ' '.join(sys.argv[1:])
+    if is_braille(string):
+
+        print(braille_to_english(string))
+
     else:
-        print(englist_to_braille(sys.argv[1]))
+
+        print(english_to_braille(string))
 if __name__ == '__main__':
     main()
